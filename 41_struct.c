72bd72567a57f37862ba022fac328d4ed325ae2e
李栋 <lidong65550@offcn.com>
2023-11-06 11:31:13 +0800

    找问题

--- 41_struct.c
+++ 41_struct.c
@@ -2,20 +2,47 @@
 // Created by ld on 2023/11/2.
 //
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+typedef struct{
+   int year;
+   int month;
+   int day;
+}BD;
 
 struct student{
     int Id;
     char name[32];
     char sex;
     int age;
+    BD birth;
 };
 
 
 int main(){
 
-    struct student lili;
+
     struct student mingming = {1001, "kakaxi", 'B', 18};
-    printf("lili info:%s\n", mingming.name);
+    printf("mingming's name is:%s\n", mingming.name);
+
+    struct student lili;
+    lili.Id = 1002;
+    strcpy(lili.name, "ÀöÀö");
+    lili.sex = 'G';
+    lili.age = 18;
+    BD bd = {2003, 6, 6};
+    lili.birth = bd;
+    printf("%d-%s: %d-%d-%d\n", lili.Id, lili.name, lili.birth.year, lili.birth.month, lili.birth.day);
+
+    struct student *s;
+    s = (struct student *)malloc(sizeof(struct student));
+    s->Id = 1003;
+    strcpy(s->name, "Ð¡S");
+    s->sex = 'B';
+    printf("%d-%s\n", s->Id, s->name);
+    free(s);
+    s = NULL;
 
 
     return 0;
---
+++ 42_link.c
@@ -0,0 +1,56 @@
+//
+// Created by ld on 2023/11/3.
+//
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct student{
+    int Id;
+    char name[32];
+    struct student *Next;
+}STU;
+
+
+void link_create(STU **p_head, STU *p_new){
+    STU *p_mov = *p_head;
+    if(*p_head == NULL){
+        *p_head = p_new;
+        p_new = NULL;
+    }else{
+        while (p_mov->Next != NULL){
+            p_mov = p_mov->Next;
+        }
+        printf("%s %s\n", p_mov->name, p_new->name);
+        p_mov->Next = p_new;
+        p_new->Next = NULL;
+    }
+}
+
+
+int main(){
+
+    STU *header = NULL, *p_new = NULL;
+    int num = 9, i;
+    char name[32];
+    for (i = 0;i < num; i++){
+        p_new = (STU*)malloc(sizeof(STU));
+
+        p_new->Id = i;
+        sprintf(name, "name_%d", i);
+        strcpy(p_new->name, name);
+
+        printf("%d: %p id(%d)\tname(%s)\tNext(%p)\n", i, p_new, p_new->Id, p_new->name, p_new->Next);
+        link_create(&header, p_new);
+
+
+    }
+
+    STU *pMov = header;
+    while (pMov->Next != NULL){
+        printf("%d-%s-%p\n", pMov->Id, pMov->name, pMov->Next);
+        pMov = pMov->Next;
+    }
+
+    return 0;
+}
